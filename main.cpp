6c0e50b540d31ad11e43c1ffa43fbb5571199581
dionysis <dionysis@mail.com>
2021-10-17 14:27:03 +0300

    working commit && git ignore

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 #include "main.h"
 
+wxIMPLEMENT_APP(MainApp);
 
 bool MainApp::OnInit() {
-    MyFrame *frame = new MyFrame(1, "...");
+    MyFrame *frame = new MyFrame(wxID_ANY, "TODO-ls");
     frame->Show(true);
-
     return true;
 }
--- mainFrame.cpp
+++ mainFrame.cpp
@@ -1,5 +1,5 @@
 #include "mainFrame.h"
 
-MyFrame::MyFrame(wxWindowID id,const wxString& name) : id(id), name(name){
-	wxFrame(NULL, id, name);
+MyFrame::MyFrame(wxWindowID id,const wxString& name) : id(id), name(name), wxFrame(NULL, id, name, wxPoint(0, 0), wxSize(300,300)){
+
 }
