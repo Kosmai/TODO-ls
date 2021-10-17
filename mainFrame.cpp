90c04be38ddf01b32c00cd8cbde518be2ffd6cd0
d-maniatakos <dionysis.maniatakos@yahoo.com>
2021-10-17 16:22:46 +0300

    basic menu bar

--- mainFrame.cpp
+++ mainFrame.cpp
@@ -1,5 +1,21 @@
 #include "mainFrame.h"
 
 MyFrame::MyFrame(wxWindowID id,const wxString& name, wxPoint point, wxSize size) : id(id), name(name), wxFrame(NULL, id, name, point, size){
+    wxMenuBar* menuBar = new wxMenuBar();
 
+    wxMenu* fileMenu = new wxMenu();
+    wxMenu* helpMenu = new wxMenu();
+
+    fileMenu->Append(wxID_NEW);
+    fileMenu->Append(wxID_OPEN);
+    fileMenu->Append(wxID_SAVE);
+    fileMenu->Append(wxID_SAVEAS);
+
+    helpMenu->Append(wxID_HELP);
+    helpMenu->Append(wxID_ABOUT);
+
+    menuBar->Append(fileMenu, _("&File"));
+    menuBar->Append(helpMenu, _("&Help"));
+
+    SetMenuBar(menuBar);
 }
